86901e1e0a375cffad232c385e19c9b1071dba03
Aditya <120359599+aditya-git16@users.noreply.github.com>
2026-04-19 04:10:20 +0530

    Add solution for numpy-create-array

---
+++ study-plans/numpy-basics/numpy-create-array/numpy-create-array.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def create_array(data):
+    """
+    Returns: 2D numpy array with dtype float64
+    """
+    np_arr = np.array(data , np.float64)
+    return np_arr
+    pass
